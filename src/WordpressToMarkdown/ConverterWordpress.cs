6a0cd39306aaecd8a6d4072a3e1fe37097f6c457
Anthony RYCK <anthony.ryck@hotmail.fr>
2022-03-13 14:31:35 +0100

    Ajout du projet

---
+++ src/WordpressToMarkdown/ConverterWordpress.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WordpressToMarkdown.Models;
+
+namespace WordpressToMarkdown
+{
+	public class ConverterWordpress
+	{
+
+
+
+
+
+		
+		public async Task<List<PostWordPress>> GetPosts(string url)
+		{
+			List<PostWordPress> posts = new List<PostWordPress>();
+
+			using (var client = new HttpClient())
+			{
+				var streamPosts = await client.GetStreamAsync(url);
+				posts = await JsonSerializer.DeserializeAsync<List<PostWordPress>>(streamPosts);
+			}
+
+			return posts;
+		}
+	}
+}
