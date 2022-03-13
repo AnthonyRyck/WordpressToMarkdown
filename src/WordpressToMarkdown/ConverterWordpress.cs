66de064bee9e91722974447f7a1a94c68fbd2c55
Anthony RYCK <anthony.ryck@hotmail.fr>
2022-03-13 15:47:48 +0100

    Ajout debut de methode

--- src/WordpressToMarkdown/ConverterWordpress.cs
+++ src/WordpressToMarkdown/ConverterWordpress.cs
@@ -9,12 +9,10 @@ namespace WordpressToMarkdown
 {
 	public class ConverterWordpress
 	{
-
-
-
-
-
+		const string RETOUR_LIGNE = "\n\n\n\n";
 		
+
+
 		public async Task<List<PostWordPress>> GetPosts(string url)
 		{
 			List<PostWordPress> posts = new List<PostWordPress>();
@@ -26,6 +24,39 @@ namespace WordpressToMarkdown
 			}
 
 			return posts;
+		}
+
+
+
+
+		private string ChangeHeader(string content)
+		{
+			return content.Replace("<h1>", MarkdownSyntax.HEADER_1)
+					.Replace("</h1>", Environment.NewLine)
+					.Replace("<h2>", MarkdownSyntax.HEADER_2)
+					.Replace("</h2>", Environment.NewLine)
+					.Replace("<h3>", MarkdownSyntax.HEADER_3)
+					.Replace("</h3>", Environment.NewLine)
+					.Replace("<h4>", MarkdownSyntax.HEADER_4)
+					.Replace("</h4>", Environment.NewLine)
+					.Replace("<h5>", MarkdownSyntax.HEADER_5)
+					.Replace("</h5>", Environment.NewLine)
+					.Replace("<h6>", MarkdownSyntax.HEADER_6)
+					.Replace("</h6>", Environment.NewLine);
+		}
+
+		private string ChangeRetourLigne(string content)
+		{
+			return content.Replace(RETOUR_LIGNE, Environment.NewLine)
+						.Replace("<br>", MarkdownSyntax.SAUT_LIGNE)
+						.Replace("<p>", Environment.NewLine)
+						.Replace("</p>", MarkdownSyntax.SAUT_LIGNE);
+		}
+
+
+		private string ChangeImage(string content)
+		{
+			return content;
 		}
 	}
 }
---
+++ src/WordpressToMarkdown/Models/MarkdownSyntax.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordpressToMarkdown.Models
+{
+	internal static class MarkdownSyntax
+	{
+		/// <summary>
+		/// Correspond à un <hx></hx>
+		/// </summary>
+		public const string HEADER_1 = "# ";
+		public const string HEADER_2 = "## ";
+		public const string HEADER_3 = "### ";
+		public const string HEADER_4 = "#### ";
+		public const string HEADER_5 = "##### ";
+		public const string HEADER_6 = "###### ";
+
+		/// <summary>
+		/// Correspond à un <br>
+		/// </summary>
+		public const string SAUT_LIGNE = ("  \r\n");
+
+		/// <summary>
+		/// Correspond au <strong>
+		/// </summary>
+		public const string BOLD = "**";
+
+		/// <summary>
+		/// Correspond au <em>
+		/// </summary>
+		public const string ITALIC = "*";
+
+		/// <summary>
+		/// Correspond au <strong><em>
+		/// </summary>
+		public const string ITALIC_BOLD = "***";
+
+		/// <summary>
+		/// Pour faire le système de point <ul><li>
+		/// </summary>
+		public const string START_UNORDORED_LIST = "* ";
+
+		/// <summary>
+		/// Correspond à <code> ou <pre>
+		/// </summary>
+		public const string CODE = "```";
+
+		public static string Image(string name, string url)
+		{
+			return "[" + name + "](" + url + ")";
+		}
+
+		// A voir pour les vidéos
+		// Exemple :
+		// [![Blinking LEDs] (http://img.youtube.com/vi/XAMVzS13HY0/0.jpg)](http://www.youtube.com/watch?v=XAMVzS13HY0 "Blinking LEDs")
+		// [![Alternate Text]({image-url})]({video-url} "Link Title")
+
+	}
+}
