cf7a1a5f1249834d1f0ae827a2d95a206def10af
AnthonyRyck <anthony.ryck@hotmail.fr>
2022-03-15 14:47:18 +0100

    Transformation dans le cas d'une vidéo

--- src/WordpressToMarkdown/ConverterWordpress.cs
+++ src/WordpressToMarkdown/ConverterWordpress.cs
@@ -15,6 +15,11 @@ namespace WordpressToMarkdown
 		/// Arrive quand il y a changement de paragraphe
 		/// </summary>
 		const string CHAGEMENT_PARAGRAPHE = "\n\n\n\n";
+		
+		/// <summary>
+		/// C'est le post en version "HTML"
+		/// </summary>
+		private HtmlDocument htmlDocPost;
 
 		public async Task<List<PostWordPress>> ConvertToObject(Stream content)
 		{
@@ -34,19 +39,19 @@ namespace WordpressToMarkdown
 			return posts;
 		}
 
-		private HtmlDocument doc;
 
 		public string ConvertToMarkdown(string contentPost, params ITransformWordpress[] transformations)
 		{
 			contentPost = WebUtility.HtmlDecode(contentPost);
-			doc = new HtmlDocument();
-			doc.LoadHtml(contentPost);
+			htmlDocPost = new HtmlDocument();
+			htmlDocPost.LoadHtml(contentPost);
 
 			contentPost = ChangeHeader(contentPost);
 			contentPost = ChangeStyle(contentPost);
 			contentPost = ChangeList(contentPost);
 			contentPost = ChangeLink(contentPost);
 			contentPost = ChangeImage(contentPost);
+			contentPost = ChangeVideo(contentPost);
 			contentPost = ChangeCodeEnLigne(contentPost);
 			contentPost = ChangeCodeBlock(contentPost);
 			contentPost = ChangeRetourLigne(contentPost);
@@ -63,7 +68,7 @@ namespace WordpressToMarkdown
 		{
 			string[] headers = new string[] { "h1", "h2", "h3", "h4", "h5", "h6" };
 
-			var lesHeaders = doc.DocumentNode.Descendants().Where(x => headers.Contains(x.Name));
+			var lesHeaders = htmlDocPost.DocumentNode.Descendants().Where(x => headers.Contains(x.Name));
 			foreach (HtmlNode? header in lesHeaders)
 			{
 				string headerSelected = string.Empty;
@@ -110,8 +115,6 @@ namespace WordpressToMarkdown
 			//		.Replace(@"<\/h6>", Environment.NewLine);
 		}
 
-		
-
 		private string ChangeRetourLigne(string content)
 		{
 			content = content.Replace(CHAGEMENT_PARAGRAPHE, string.Empty)
@@ -127,7 +130,7 @@ namespace WordpressToMarkdown
 			// Codes en ligne
 			// Exemple :
 			// <code data-enlighter-language="csharp" class="EnlighterJSRAW">LE code</code>
-			var codesLine = doc.DocumentNode.Descendants().Where(d => d.Name == "code");
+			var codesLine = htmlDocPost.DocumentNode.Descendants().Where(d => d.Name == "code");
 
 			foreach (var code in codesLine)
 			{
@@ -151,7 +154,7 @@ namespace WordpressToMarkdown
 		{
 			// Exemple :
 			// <div class="wp-block-image"><figure class="aligncenter size-large"><img src="https://spectreconsole.net/assets/images/example.png" alt=""/><figcaption>Image de spectreconsole.net</figcaption></figure></div>
-			var lesImages = doc.DocumentNode.Descendants().Where(x => x.Name == "div" 
+			var lesImages = htmlDocPost.DocumentNode.Descendants().Where(x => x.Name == "div" 
 																	&& x.HasAttributes 
 																	&& x.Attributes["class"].Value == "wp-block-image");
 			foreach (var item in lesImages)
@@ -169,7 +172,36 @@ namespace WordpressToMarkdown
 					content = content.Remove(indexStart, indexEnd - indexStart + div.Length);
 					content = content.Insert(indexStart, "METTRE-TAG-IMG-ICI");
 
-					content = content.Replace("METTRE-TAG-IMG-ICI", image.OuterHtml);
+					content = content.Replace("METTRE-TAG-IMG-ICI", image.OuterHtml + Environment.NewLine);
+				}
+			}
+			return content;
+		}
+
+		private string ChangeVideo(string content)
+		{
+			string URL_VIDEO = "METTRE-URL-VIDEO";
+			string figureStart = @"<figure class=""wp-block-video aligncenter"">";
+			string figureEnd = "</figure>";
+
+			// Exemple :
+			// <figure class="wp-block-video aligncenter"><video controls src="https://spectreconsole.net/assets/images/table.webm"></video><figcaption>Image de spectreconsole.net</figcaption></figure>
+			var lesImages = htmlDocPost.DocumentNode.Descendants().Where(x => x.Name == "figure"
+																	&& x.HasAttributes
+																	&& x.Attributes["class"].Value.Contains("wp-block-video"));
+			foreach (var item in lesImages)
+			{
+				// récup du tag img
+				var videoTag = item.Descendants().FirstOrDefault(x => x.Name == "video");
+				if (videoTag != null)
+				{
+					int indexStart = content.IndexOf(figureStart);
+					int indexEnd = content.IndexOf(figureEnd);
+
+					content = content.Remove(indexStart, indexEnd - indexStart + figureEnd.Length);
+					content = content.Insert(indexStart, URL_VIDEO);
+
+					content = content.Replace(URL_VIDEO, "[VIDEO](" + videoTag.Attributes["src"].Value + ")" + Environment.NewLine);
 				}
 			}
 			return content;
@@ -179,7 +211,7 @@ namespace WordpressToMarkdown
 		{
 			// Exemple :
 			// <a rel="noreferrer noopener" href="https://www.nuget.org/packages/Spectre.Console/0.43.1-preview.0.43" target="_blank">GitHub : Spectre.Console</a>
-			var lesLinks = doc.DocumentNode.Descendants().Where(x => x.Name == "a");
+			var lesLinks = htmlDocPost.DocumentNode.Descendants().Where(x => x.Name == "a");
 			foreach (var item in lesLinks)
 			{
 				content = content.Replace(item.OuterHtml, "[" + item.InnerHtml + "](" + item.Attributes["href"].Value + ")");
@@ -192,7 +224,7 @@ namespace WordpressToMarkdown
 		{
 			// ### Exemple AVEC language sélectionné:
 			// <pre class="EnlighterJSRAW" data-enlighter-language="csharp" data-enlighter-theme="" data-enlighter-highlight="" data-enlighter-linenumbers="" data-enlighter-lineoffset="" data-enlighter-title="" data-enlighter-group="">using Spectre.Console;
-			var preNode = doc.DocumentNode.Descendants().Where(d => d.Name == "pre");
+			var preNode = htmlDocPost.DocumentNode.Descendants().Where(d => d.Name == "pre");
 
 			foreach (var item in preNode)
 			{
